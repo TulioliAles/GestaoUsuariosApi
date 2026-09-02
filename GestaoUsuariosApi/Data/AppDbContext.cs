8a717b02f658c19b4b44cc3dff138748ba091010
Alessandro Tulioli <desmecad@hotmail.com>
2026-09-02 10:26:00 -0300

    Criação das configurações de acesso ao bando de dados

---
+++ GestaoUsuariosApi/Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using GestaoUsuariosApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace GestaoUsuariosApi.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) :
+            base(options)
+        {            
+        }
+
+        public DbSet<User> Usuarios { get; set; }
+    }
+}
---
+++ GestaoUsuariosApi/Models/User.cs
@@ -0,0 +1,11 @@
+﻿namespace GestaoUsuariosApi.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; } = string.Empty;
+        public string Sobrenome { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string Cpf { get; set; } = string.Empty;
+    }
+}
